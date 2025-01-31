d42e45d6f0f8b114873f23418ebcbe887687fd1a
ivdznk <dzizenko.i@gmail.com>
2025-01-31 11:46:28 +0200

    Created plugin-gmxbt

---
+++ packages/plugin-gmxbt/src/services/twitterPostJob.ts
@@ -0,0 +1,82 @@
+import cron from "node-cron";
+
+import {
+    elizaLogger,
+    IAgentRuntime,
+    Service,
+    ServiceType,
+} from "@elizaos/core";
+
+export interface ITwitterPostJobService extends Service {
+    runCron(): Promise<void>;
+}
+
+export class TwitterPostJobService
+    extends Service
+    implements ITwitterPostJobService
+{
+    private runtime: IAgentRuntime;
+
+    constructor() {
+        super();
+
+        this.runCron();
+    }
+
+    static get serviceType(): ServiceType {
+        return ServiceType.TEXT_GENERATION;
+    }
+
+    get serviceType(): ServiceType {
+        return ServiceType.TEXT_GENERATION;
+    }
+
+    async initialize(_runtime: IAgentRuntime): Promise<void> {
+        this.runtime = _runtime;
+    }
+
+    async runCron(): Promise<void> {
+        cron.schedule("*/1 * * * *", async () => {
+            elizaLogger.log(
+                "Run updatePortfolioTweet at",
+                new Date().toUTCString()
+            );
+            await this.updatePortfolioTweet();
+        });
+
+        elizaLogger.log("TwitterPostJobService cron has been started");
+    }
+
+    async updatePortfolioTweet(): Promise<void> {
+        const twitterClient = this.runtime?.clients?.twitter;
+
+        if (!twitterClient) {
+            return;
+        }
+
+        const agentId = this.runtime.agentId;
+
+        try {
+            const url = "http://localhost:3000/" + agentId + "/message";
+            const body = JSON.stringify({
+                text: "Post the fund portfolio for gmxbt",
+                userId: "user-fund-update-twitter",
+                roomId: "default-room-twitter-cron-" + agentId,
+            });
+
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: body,
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+        } catch (error) {
+            elizaLogger.error("Error updating portfolio twitter", error);
+        }
+    }
+}
